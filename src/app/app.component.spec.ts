b0c2b48bf1718d0fdb42b579b7b2b263b2f63409
ajmalsabith <ajmalsabith444@gmail.com>
2024-04-15 16:02:41 +0530

    first

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'mechine-test'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('mechine-test');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mechine-test app is running!');
+  });
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  registerform!:FormGroup
+  ishoww=false
+  dataArray:any[]=[]
+  iseditbut:boolean=false
+  i:number=0
+
+  constructor(){}
+  ngOnInit(): void {
+    this.registerform= new FormGroup({
+      name: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required,Validators.email]),
+      phone: new FormControl('',[Validators.required,Validators.pattern(/^\d{10}$/)]),
+      address: new FormControl('',[Validators.required]),
+      gender: new FormControl('',[Validators.required]),
+    })
+  }
+
+  submit(){
+    const user= this.registerform.getRawValue()
+    this.dataArray.push(user)
+    this.registerform.reset()
+    this.ishoww=false
+  }
+
+  removepopup(){
+    this.ishoww=false
+  }
+  editset(i:number){
+    this.ishoww=true
+    this.iseditbut=true
+    const editdata= this.dataArray[i]
+    this.i=i
+    this.registerform= new FormGroup({
+      name: new FormControl(editdata.name,[Validators.required]),
+      email: new FormControl(editdata.email,[Validators.required,Validators.email]),
+      phone: new FormControl(editdata.phone,[Validators.required,Validators.pattern(/^\d{10}$/)]),
+      address: new FormControl(editdata.address,[Validators.required]),
+      gender: new FormControl(editdata.gender,[Validators.required]),
+    })
+  }
+
+  get email(){
+    return this.registerform.get('email')
+  }
+  get name(){
+    return this.registerform.get('name')
+  }
+  get phone(){
+    return this.registerform.get('phone')
+  }
+  get address(){
+    return this.registerform.get('address')
+  }
+
+  showpopup(){
+    this.ishoww=true
+  }
+
+  editconfirm(){
+    const edituser= this.registerform.getRawValue()
+    this.dataArray[this.i]=edituser
+    this.registerform.reset()
+    this.iseditbut=false
+    this.ishoww=false
+  }
+
+ 
+}
